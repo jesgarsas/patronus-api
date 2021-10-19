7e919b1049cdcb698176c6a6fc7ecd95ebd85796
jesgarsas <jesgarsas@gmai.com>
2021-10-19 23:37:56 +0200

    Añadido descarga de proyecto

--- src/main/java/com/upv/jesgarsas/patronusapi/app/controller/ProyectoController.java
+++ src/main/java/com/upv/jesgarsas/patronusapi/app/controller/ProyectoController.java
@@ -1,8 +1,13 @@
 package com.upv.jesgarsas.patronusapi.app.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,22 +15,44 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.upv.jesgarsas.patronusapi.app.model.dto.ProyectoDTO;
+import com.upv.jesgarsas.patronusapi.app.model.entity.Proyecto;
 import com.upv.jesgarsas.patronusapi.app.service.ProyectoService;
 
 @Controller
 @RequestMapping(value = "/proyecto")
 public class ProyectoController {
-	
+
 	@Autowired
 	private ProyectoService proyectoService;
-	
+
 	@GetMapping(value = "/{idPatron}")
-	public ResponseEntity<List<ProyectoDTO>> findByPatron(@PathVariable(name = "idPatron", required = true) Integer idPatron) {
+	public ResponseEntity<List<ProyectoDTO>> findByPatron(
+			@PathVariable(name = "idPatron", required = true) Integer idPatron) {
 		return ResponseEntity.ok(proyectoService.findByPatron(idPatron));
 	}
-	
+
 	@GetMapping(value = "/download/{id}")
-	public ResponseEntity<ProyectoDTO> findById(@PathVariable(name = "id", required = true) Integer id) {
-		return ResponseEntity.ok(proyectoService.findById(id));
+	public ResponseEntity<InputStreamResource> findById(@PathVariable(name = "id", required = true) Integer id)
+			throws Exception {
+
+		Proyecto proyecto = proyectoService.findByIdEntity(id);
+		File file = proyectoService.getFileById(proyecto);
+		if (file != null) {
+			InputStreamResource in;
+			try {
+				in = new InputStreamResource(new FileInputStream(file));
+				return ResponseEntity.ok()
+						.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + proyecto.getName())
+						.contentLength(file.length()) //
+						.body(in);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			throw new Exception("El id proporcionado no esta vinculado a ningún proyecto");
+		}
+		return null;
+
 	}
+
 }
--- src/main/java/com/upv/jesgarsas/patronusapi/app/service/ProyectoService.java
+++ src/main/java/com/upv/jesgarsas/patronusapi/app/service/ProyectoService.java
@@ -1,11 +1,15 @@
 package com.upv.jesgarsas.patronusapi.app.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import com.upv.jesgarsas.patronusapi.app.model.dto.ProyectoDTO;
 import com.upv.jesgarsas.patronusapi.app.model.entity.Patron;
 import com.upv.jesgarsas.patronusapi.app.model.entity.Proyecto;
@@ -14,36 +18,69 @@ import com.upv.jesgarsas.patronusapi.app.service.mapper.ProyectoMapper;
 
 @Service
 public class ProyectoService {
-	
+
 	@Autowired
 	private ProyectoMapper proyectoMapper;
-	
+
 	@Autowired
 	private ProyectoRepository proyectoRepository;
-	
+
+	@Value("${app.patronus.download.projects}")
+	private String urlDownloadProyecto;
+
 	public List<ProyectoDTO> findByPatron(Integer idPatron) {
 		List<ProyectoDTO> proyectos = proyectoMapper.toListDto(proyectoRepository.findAllByPatronId(idPatron));
+		proyectos.forEach(proyecto -> {
+			proyecto.setLink(getUrlProyecto(proyecto));
+		});
 		return proyectos == null ? new ArrayList<ProyectoDTO>() : proyectos;
 	}
-	
+
 	public List<Proyecto> findByPatron(Patron patron) {
 		return proyectoRepository.findAllByPatronId(patron.getId());
 	}
-	
+
 	public ProyectoDTO findById(Integer id) {
 		Proyecto proyecto = proyectoRepository.findById(id).orElse(null);
-		if(proyecto != null) {
+		if (proyecto != null) {
 			return proyectoMapper.toDto(proyecto);
 		}
 		return null;
 	}
-	
+
+	public Proyecto findByIdEntity(Integer id) {
+		return proyectoRepository.findById(id).orElse(null);
+	}
+
 	public void save(Proyecto proyecto, Patron patron) {
 		proyecto.setPatron(patron);
 		proyectoRepository.save(proyecto);
 	}
-	
+
 	public void delete(Proyecto proyecto) {
 		proyectoRepository.delete(proyecto);
+	}
+
+	public File getFileById(Proyecto proyecto) {
+		if (proyecto != null) {
+			Path path;
+			try {
+				path = Files.createTempFile("project", null);
+				Files.write(path, proyecto.getArchivo());
+				File file = new File(path.toUri());
+				return file;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	// tiene que tener una id, devuelve "" si no
+	private String getUrlProyecto(ProyectoDTO proyecto) {
+		if (proyecto.getId() != null) {
+			return urlDownloadProyecto.replace("{id}", String.valueOf(proyecto.getId()));
+		}
+		return "";
 	}
 }
